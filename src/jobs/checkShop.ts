1dac6668649d0c750a9dc097f7a4c6c43a5b44bc
haspadarz-padmensku <gonchds@gmail.com>
2023-11-23 15:50:20 +0300

    fix italic syntax

--- src/jobs/checkShop.ts
+++ src/jobs/checkShop.ts
@@ -11,9 +11,9 @@ const checkShop = async (bot: TelegramBot) => {
 
     for await (const chatId of subscriptions) {
       for await (const product of newProducts) {
-        const caption = `${product.name}\n\n${product.description}\n\n${product.price}\n\n<em>обновлено ${new Date(
+        const caption = `${product.name}\n\n${product.description}\n\n${product.price}\n\n*Обновлено ${new Date(
           product.date ?? '11/23/2023',
-        ).toLocaleString()}</em>`;
+        ).toLocaleString()}*`;
 
         bot.sendPhoto(chatId, product.imageUrl, {
           caption,
